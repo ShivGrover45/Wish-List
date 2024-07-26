e21cebc0157eb33a4e6f4ab6b4bd38d28541b69d
ShivGrover45 <codershiv16@gmail.com>
2024-07-27 01:27:10 +0530

    changes added

--- app/src/main/java/com/example/wishlist/data/WishRepository.kt
+++ app/src/main/java/com/example/wishlist/data/WishRepository.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 class WishRepository(private val wishDao: WishDao) {
 
     suspend fun addWish(wish:Wish){
-        wishDao.addWish(wish)
+        wishDao.addAWish(wish)
     }
 
     suspend fun updateWish(wish: Wish){
